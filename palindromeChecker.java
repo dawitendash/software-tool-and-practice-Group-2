e0e22916c21fef4e756b90262b6713919e7d03e1
Dawit <dawitendashaw75@gmail.com>
2024-06-05 21:43:10 +0200

    Dawit modified this file

--- palindromeChecker.java
+++ palindromeChecker.java
@@ -1,18 +1,16 @@
 package tools;
 
 public class palindromeChecker {
-	public String reverse(String s) {
+	public String reverse(String s){
         String reversed = "";
         for(int i = 0; i < s.length(); i++) {
             reversed = s.charAt(i) + reversed;
         }
-        return reversed;
+            return reversed;
     }
-    
     public boolean isPalindrome(String s) {
         return s.equals(reverse(s));
     }  
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- palindromeCheckerTest.java
+++ palindromeCheckerTest.java
@@ -3,9 +3,7 @@ package tools;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
 class palindromeCheckerTest {
-
 palindromeChecker palindromeChecker = new palindromeChecker();
     
     @Test
@@ -13,7 +11,6 @@ palindromeChecker palindromeChecker = new palindromeChecker();
         assertEquals("level", palindromeChecker.reverse("level"));
         assertEquals("abe", palindromeChecker.reverse("eba"));
     }
-    
     @Test
     public void testIsPalindrome() {
         assertEquals(true, palindromeChecker.isPalindrome("level"));
